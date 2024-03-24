541912a80718f92ddf7b63ceed38efdb6e5ec5ac
Ryder Belserion <no-reply@ryderbelserion.com>
2024-03-24 14:17:26 -0400

    Add extra formats

--- src/main/java/com/ryderbelserion/discordchat/platform/impl/Locale.java
+++ src/main/java/com/ryderbelserion/discordchat/platform/impl/Locale.java
@@ -1,10 +1,9 @@
 package com.ryderbelserion.discordchat.platform.impl;
 
+import ch.jalu.configme.Comment;
 import ch.jalu.configme.SettingsHolder;
 import ch.jalu.configme.properties.Property;
-
 import java.util.List;
-
 import static ch.jalu.configme.properties.PropertyInitializer.newListProperty;
 import static ch.jalu.configme.properties.PropertyInitializer.newProperty;
 
@@ -19,7 +18,24 @@ public class Locale implements SettingsHolder {
             " <red>/discordchat reload <gray>- <gold>Reloads the plugin."
     ));
 
+    @Comment("The message sent when the server starts up.")
     public static final Property<String> server_started = newProperty("server.started", "The server has started.");
 
+    @Comment("The message sent when the server shuts down.")
     public static final Property<String> server_shutdown = newProperty("server.shutdown", "The server has shutdown.");
+
+    @Comment("The color of the player join embed.")
+    public static final Property<String> player_join_color = newProperty("player.embeds.join.color", "#0eeb6a");
+
+    @Comment("The color of the player quit embed.")
+    public static final Property<String> player_quit_color = newProperty("player.embeds.quit.color", "#e0240b");
+
+    @Comment("The title format of the embed.")
+    public static final Property<String> player_msg_title = newProperty("player.embeds.msg.format.title", "{username}");
+
+    @Comment("The format of messages sent to discord.")
+    public static final Property<String> player_msg_format = newProperty("player.embeds.msg.format.description", "{message}");
+
+    @Comment("The color of the message embed.")
+    public static final Property<String> player_msg_color = newProperty("player.embeds.msg.color", "#bff7fd");
 }
