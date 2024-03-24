d9f38334fd04ae63e914a252f1acab62021bda05
Ryder Belserion <no-reply@ryderbelserion.com>
2024-03-24 14:17:56 -0400

    Update chat event

--- src/main/java/com/ryderbelserion/discordchat/listeners/PlayerChatEvent.java
+++ src/main/java/com/ryderbelserion/discordchat/listeners/PlayerChatEvent.java
@@ -1,7 +1,10 @@
 package com.ryderbelserion.discordchat.listeners;
 
+import ch.jalu.configme.SettingsManager;
 import com.ryderbelserion.discordchat.DiscordChat;
+import com.ryderbelserion.discordchat.platform.ConfigManager;
 import com.ryderbelserion.discordchat.platform.discord.DiscordBot;
+import com.ryderbelserion.discordchat.platform.impl.Locale;
 import io.papermc.paper.event.player.AsyncChatEvent;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
@@ -13,6 +16,10 @@ public class PlayerChatEvent implements Listener {
 
     private final @NotNull DiscordChat plugin = JavaPlugin.getPlugin(DiscordChat.class);
 
+    private final @NotNull ConfigManager configManager = this.plugin.getConfigManager();
+
+    private final @NotNull SettingsManager locale = this.configManager.getLocale();
+
     private final @NotNull DiscordBot bot = this.plugin.getDiscordBot();
 
     @EventHandler
@@ -21,8 +28,10 @@ public class PlayerChatEvent implements Listener {
 
         String message = event.signedMessage().message();
 
-        String format = "{name} > {message}";
+        String format = this.locale.getProperty(Locale.player_msg_format);
+        String title = this.locale.getProperty(Locale.player_msg_title);
+        String color = this.locale.getProperty(Locale.player_msg_color);
 
-        this.bot.sendDiscordMessage(format.replaceAll("\\{name}", player.getName()).replaceAll("\\{message}", message));
+        this.bot.sendDiscordMessage(player, title.replaceAll("\\{username}", player.getName()), format.replaceAll("\\{message}", message), color);
     }
 }
