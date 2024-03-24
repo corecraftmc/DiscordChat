398fc6dfba0fb5184a6d71e3fdf369d7260808b6
Ryder Belserion <no-reply@ryderbelserion.com>
2024-03-24 17:31:26 -0400

    Update locale layout

--- src/main/java/com/ryderbelserion/discordchat/platform/impl/Locale.java
+++ src/main/java/com/ryderbelserion/discordchat/platform/impl/Locale.java
@@ -9,10 +9,10 @@ import static ch.jalu.configme.properties.PropertyInitializer.newProperty;
 
 public class Locale implements SettingsHolder {
 
-    public static final Property<String> reload_plugin = newProperty("root.reload-plugin", "{prefix}<red>You have reloaded the plugin.");
+    public static final Property<String> reloaded_plugin = newProperty("root.reload-plugin", "{prefix}<red>You have reloaded the plugin.");
 
-    public static final Property<List<String>> help = newListProperty("root.help-menu", List.of(
-            "<red>DiscordChat Help Menu",
+    public static final Property<List<String>> player_help = newListProperty("root.player-help", List.of(
+            "<red>DiscordChat Player Help",
             "",
             " <red>/discordchat <gray>- <gold>Shows this menu.",
             " <red>/discordchat reload <gray>- <gold>Reloads the plugin."
@@ -25,21 +25,15 @@ public class Locale implements SettingsHolder {
     public static final Property<String> server_shutdown = newProperty("server.shutdown", "The server has shutdown.");
 
     @Comment("The color of the player join embed.")
-    public static final Property<String> player_join_color = newProperty("player.embeds.join.color", "#0eeb6a");
+    public static final Property<String> player_join_color = newProperty("player.embeds.join-format.color", "#0eeb6a");
 
-    public static final Property<String> player_join_title = newProperty("player.embeds.join.format.title", "{username} has joined the server.");
+    public static final Property<String> player_join_title = newProperty("player.embeds.join-format.title", "{username} has joined the server.");
 
     @Comment("The color of the player quit embed.")
-    public static final Property<String> player_quit_color = newProperty("player.embeds.quit.color", "#e0240b");
+    public static final Property<String> player_quit_color = newProperty("player.embeds.quit-format.color", "#e0240b");
 
-    public static final Property<String> player_quit_title = newProperty("player.embeds.quit.format.title", "{username} has left the server.");
+    public static final Property<String> player_quit_title = newProperty("player.embeds.quit-format.title", "{username} has left the server.");
 
-    @Comment("The title format of the embed.")
-    public static final Property<String> player_msg_title = newProperty("player.embeds.msg.format.title", "{username}");
-
-    @Comment("The format of messages sent to discord.")
-    public static final Property<String> player_msg_format = newProperty("player.embeds.msg.format.description", "{message}");
-
-    @Comment("The color of the message embed.")
-    public static final Property<String> player_msg_color = newProperty("player.embeds.msg.color", "#bff7fd");
+    @Comment("The format of the message sent to discord.")
+    public static final Property<String> player_chat_format = newProperty("player.chat.format", "{username} » {message}");
 }
