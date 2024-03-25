c8dee5fe656d1cc8fd170e66b44e2c4488bd8363
Ryder Belserion <no-reply@ryderbelserion.com>
2024-03-24 23:28:14 -0400

    Send messages with roles/colors in-game

--- src/main/java/com/ryderbelserion/discordchat/platform/discord/listeners/DiscordChatListener.java
+++ src/main/java/com/ryderbelserion/discordchat/platform/discord/listeners/DiscordChatListener.java
@@ -2,12 +2,20 @@ package com.ryderbelserion.discordchat.platform.discord.listeners;
 
 import com.ryderbelserion.discordchat.DiscordChat;
 import com.ryderbelserion.discordchat.platform.discord.DiscordBot;
+import com.ryderbelserion.discordchat.platform.discord.api.utils.ColorUtils;
+import com.ryderbelserion.discordchat.platform.discord.api.utils.RoleUtils;
+import com.ryderbelserion.discordchat.platform.impl.enums.Messages;
+import net.dv8tion.jda.api.entities.Member;
 import net.dv8tion.jda.api.entities.Message;
+import net.dv8tion.jda.api.entities.Role;
 import net.dv8tion.jda.api.entities.User;
 import net.dv8tion.jda.api.events.message.MessageReceivedEvent;
 import net.dv8tion.jda.api.hooks.ListenerAdapter;
 import org.bukkit.plugin.java.JavaPlugin;
 import org.jetbrains.annotations.NotNull;
+import java.awt.Color;
+import java.util.HashMap;
+import java.util.Map;
 
 public class DiscordChatListener extends ListenerAdapter {
 
@@ -24,9 +32,39 @@ public class DiscordChatListener extends ListenerAdapter {
 
         if (user.isBot() || user.isSystem()) return;
 
-        String userName = user.getEffectiveName();
+        Member member = event.getMember();
+
+        if (member == null) return;
+
+        String userName = member.getEffectiveName();
         String rawMessage = message.getContentRaw();
 
-        this.bot.sendMinecraftMessage("{user} >> {message}".replaceAll("\\{user}", userName).replaceAll("\\{message}", rawMessage));
+        Map<String, String> placeholders = new HashMap<>();
+        placeholders.put("{username}", userName);
+        placeholders.put("{message}", rawMessage);
+
+        Role role = RoleUtils.getHighestRole(member);
+
+        if (role != null) {
+            placeholders.put("{role}", role.getName());
+
+            Color color = role.getColor();
+
+            if (color != null) {
+                placeholders.put("{color}", "<#" + ColorUtils.toHex(color) + ">");
+            } else {
+                Role highestRole = RoleUtils.getHighestRoleWithColor(member);
+
+                if (highestRole != null) {
+                    Color highestColor = highestRole.getColor();
+
+                    if (highestColor != null) {
+                        placeholders.put("{color}", "<#" + ColorUtils.toHex(highestColor) + ">");
+                    }
+                }
+            }
+        }
+
+        this.bot.sendMinecraftMessage(Messages.discord_chat_format.getDiscordMessage(placeholders));
     }
 }
