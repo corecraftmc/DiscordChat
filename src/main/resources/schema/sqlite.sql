97112df2edaba1bf19d9a1feed8f70c46038cebd
Ryder Belserion <no-reply@ryderbelserion.com>
2024-03-27 15:09:24 -0400

    Update schema

--- src/main/resources/schema/sqlite.sql
+++ src/main/resources/schema/sqlite.sql
@@ -1,5 +1,11 @@
-CREATE TABLE `{prefix}users` (
+CREATE TABLE `{prefix}players` (
   `uuid` VARCHAR(36) NOT NULL,
-  `id` VARCHAR(18) NOT NULL,
   PRIMARY KEY (`uuid`)
+);
+
+CREATE TABLE `{prefix}users` (
+  `id` VARCHAR(18),
+   PRIMARY KEY (`id`),
+   CONSTRAINT FK_UUID FOREIGN KEY (id)
+   REFERENCES `{prefix}players` (`uuid`)
 );
