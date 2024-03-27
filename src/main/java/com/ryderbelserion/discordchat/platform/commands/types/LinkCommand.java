1099a923e4b3a88436654ed63aee5503ad94e209
Ryder Belserion <no-reply@ryderbelserion.com>
2024-03-27 00:52:53 -0400

    Setup link command ingame

--- src/main/java/com/ryderbelserion/discordchat/platform/commands/types/LinkCommand.java
+++ src/main/java/com/ryderbelserion/discordchat/platform/commands/types/LinkCommand.java
@@ -1,7 +1,8 @@
 package com.ryderbelserion.discordchat.platform.commands.types;
 
 import com.ryderbelserion.discordchat.platform.commands.BaseCommand;
-import com.ryderbelserion.discordchat.platform.impl.storage.Storage;
+import com.ryderbelserion.discordchat.platform.impl.cache.CacheManager;
+import com.ryderbelserion.discordchat.platform.utils.StringUtils;
 import dev.triumphteam.cmd.bukkit.annotation.Permission;
 import dev.triumphteam.cmd.core.annotations.Command;
 import org.bukkit.entity.Player;
@@ -11,13 +12,27 @@ import java.util.UUID;
 
 public class LinkCommand extends BaseCommand {
 
-    private final @NotNull Storage storage = this.plugin.getStorage();
+    private final @NotNull CacheManager cacheManager = this.plugin.getCacheManager();
 
     @Command("link")
     @Permission(value = "discordchat.link", def = PermissionDefault.OP)
     public void link(Player player) {
         UUID uuid = player.getUniqueId();
 
-        this.storage.createUser(uuid);
+        if (this.cacheManager.hasUser(uuid)) {
+            //todo() don't generate a code if user already exists.
+            return;
+        }
+
+        String code = StringUtils.generate();
+
+        if (this.cacheManager.getCode(code) != null) {
+            //todo() send message if code already exists.
+            return;
+        }
+
+        this.cacheManager.addUser(uuid, code);
+
+        player.sendMessage("This is your code: " + code);
     }
 }
