c449abe7e753d4ced2ff3672bb000e60e746b2a2
Ryder Belserion <no-reply@ryderbelserion.com>
2024-03-27 00:54:13 -0400

    Initialize cache manager

--- src/main/java/com/ryderbelserion/discordchat/DiscordChat.java
+++ src/main/java/com/ryderbelserion/discordchat/DiscordChat.java
@@ -3,18 +3,22 @@ package com.ryderbelserion.discordchat;
 import com.ryderbelserion.discordchat.platform.CommandManager;
 import com.ryderbelserion.discordchat.platform.ConfigManager;
 import com.ryderbelserion.discordchat.platform.discord.DiscordBot;
+import com.ryderbelserion.discordchat.platform.impl.cache.CacheManager;
 import com.ryderbelserion.discordchat.platform.impl.storage.Storage;
 import com.ryderbelserion.discordchat.platform.impl.storage.StorageFactory;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public class DiscordChat extends JavaPlugin {
 
+    private CacheManager cacheManager;
     private Storage storage;
     private DiscordBot bot;
 
     @Override
     public void onEnable() {
         ConfigManager.load();
+
+        this.cacheManager = new CacheManager();
 
         this.storage = new StorageFactory().getInstance();
 
@@ -29,6 +33,10 @@ public class DiscordChat extends JavaPlugin {
         this.storage.stop();
 
         this.bot.stop();
+    }
+
+    public CacheManager getCacheManager() {
+        return this.cacheManager;
     }
 
     public Storage getStorage() {
